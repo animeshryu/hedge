c5b8b245a037d4c06db1a47f028f5a62e1a7aa66
animeshryu <animeshryu@gmail.com>
2024-02-02 22:42:30 +0530

    modified:   app/ui/dashboard/navbar/navbar.jsx

--- app/ui/dashboard/navbar/navbar.jsx
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -10,6 +10,8 @@ import {
   MdSearch,
 } from "react-icons/md";
 
+import { FaDiscord } from "react-icons/fa6";
+
 const Navbar = () => {
   const pathName = usePathname()
   return (
@@ -23,7 +25,9 @@ const Navbar = () => {
         <div className={styles.icon}>
           <MdOutlineChat size={20}/>
           <MdNotifications size={20}/>
-          <MdPublic  size={20}/>
+          <a href='https://discord.gg/QUW3Yah5aT'>
+          <FaDiscord size={20}/>
+          </a>
         </div>
       </div>
     </div>
